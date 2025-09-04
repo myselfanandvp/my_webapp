d6f288315e3713ae5073f167f284703bcf3aca88
myselfanandvp <mailanandvp@gmail.com>
2025-09-04 05:58:47 +0530

    updated the database

--- wavelift/settings.py
+++ wavelift/settings.py
@@ -192,12 +192,12 @@ WSGI_APPLICATION = 'wavelift.wsgi.application'
 
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # DATABASES = {
